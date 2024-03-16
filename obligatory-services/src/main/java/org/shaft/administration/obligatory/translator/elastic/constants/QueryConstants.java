431639f285f436d068950e9104c39ab01d03120a
tejas <tejas.birje7@gmail.com>
2024-03-17 02:46:35 +0530

    Query enhanced to cuport common_props

--- obligatory-services/src/main/java/org/shaft/administration/obligatory/translator/elastic/constants/QueryConstants.java
+++ obligatory-services/src/main/java/org/shaft/administration/obligatory/translator/elastic/constants/QueryConstants.java
@@ -4,6 +4,13 @@ public class QueryConstants {
   public static final String VALUE = "v";
   public static final String GTE = "gte";
   public static final String LTE = "lte";
+  public static final String COMMON_PROPS = "commonProp";
+  public static final String WHO_DID = "whoDid";
+  public static final String DID_NOT = "didNot";
+  public static final String EVENT_BASED_FILTER = "e.";
+  public static final String EVENT_ID_BASED_FILTER = "e.eid";
+  public static final String PROP_BASED_FILTER = "prop.";
   public static final int INTEGER_ORDINAL = 1;
+  public static final int STRING_ORDINAL = 0;
   public static final int DATE_RANGE_BUCKETS = 12;
 }
--- obligatory-services/src/main/java/org/shaft/administration/obligatory/translator/elastic/services/ElasticQueryGenerator.java
+++ obligatory-services/src/main/java/org/shaft/administration/obligatory/translator/elastic/services/ElasticQueryGenerator.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.shaft.administration.obligatory.translator.elastic.constants.QueryConstants;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -17,7 +18,7 @@ import java.util.stream.Collectors;
 import static org.shaft.administration.obligatory.translator.elastic.constants.QueryConstants.*;
 
 /**
- *  This class generates a aggregation query which will give results of total users performing `x` no. of events
+ *  This class generates an aggregation query which will give results of total users performing `x` no. of events
  *  `user_count` will be number of users
  *  `graph_plot` will have buckets of date range
  *  Never compare user count documents with graph_plot numbers in bucket for that time range
@@ -44,7 +45,7 @@ public class ElasticQueryGenerator {
             qG.mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
             qG.mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
             ObjectNode o = qG.mapper.readValue("{\"whoDid\":[[{\"e\":0,\"fe\":false,\"o\":\"\",\"f\":\"\",\"v\":\"\",\"sT\":1635705000,\"eT\":1636137000},{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"nm\",\"v\":[\"Chicken Handi\",\"Veg Pulav\"],\"sT\":1635705000,\"eT\":1636137000},{\"e\":2,\"fe\":true,\"o\":\"GTE\",\"f\":\"p\",\"v\":\"20\",\"sT\":1635705000,\"eT\":1636137000}],[{\"e\":0,\"fe\":false,\"o\":\"\",\"f\":\"\",\"v\":\"\",\"sT\":1635705000,\"eT\":1636137000}]],\"didNot\":[],\"commonProp\":[]}",ObjectNode.class);
-            ObjectNode i = qG.mapper.readValue("{\"whoDid\":[[{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"cg\",\"v\":\"2343\",\"sT\":1670736428,\"eT\":1670736428,\"dt\":\"Integer\"},{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"cg\",\"v\":\"f676,dfsf,sf,sdfsdf\",\"sT\":1670736440,\"eT\":1670736440,\"dt\":\"String\"}]],\"didNot\":[],\"commonProp\":[]}",ObjectNode.class);
+            ObjectNode i = qG.mapper.readValue("{\"whoDid\":[[{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"cg\",\"v\":\"2343\",\"sT\":1670736428,\"eT\":1670736428,\"dt\":1},{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"cg\",\"v\":\"f676,dfsf,sf,sdfsdf\",\"sT\":1670736440,\"eT\":1670736440,\"dt\":0}]],\"didNot\":[],\"commonProp\":[[{\"e\":1,\"fe\":true,\"o\":\"EQ\",\"f\":\"email\",\"v\":\"tejas.birje7@gmail.com\",\"dt\":0}]]}",ObjectNode.class);
             ObjectNode k = qG.prepareAnalyticsQuery(i,true);
 
             String json = qG.mapper.writerWithDefaultPrettyPrinter().writeValueAsString(k);
@@ -54,50 +55,30 @@ public class ElasticQueryGenerator {
         }
     }
 
+    private String getQueryKey(String key) {
+        switch (key) {
+            case COMMON_PROPS:
+                return COMMON_PROPS;
+            case DID_NOT:
+                return DID_NOT;
+            case WHO_DID:
+                return WHO_DID;
+        }
+        return "";
+    }
+
     private ObjectNode getEventQuery(int eventId) {
         return mapper.createObjectNode().set("term",
-          mapper.createObjectNode().set("e.eid",
+          mapper.createObjectNode().set(EVENT_ID_BASED_FILTER,
             mapper.createObjectNode()
               .put("value", eventId)));
     }
 
-    private ObjectNode getRangeQuery(ObjectNode query) {
-        return mapper.createObjectNode().set("range",
-          mapper.createObjectNode().set("e.".concat(query.get("f").textValue()),
-            mapper.createObjectNode().put(query.get("o").textValue().toLowerCase(),query.get(VALUE).asInt())));
-    }
 
     private ObjectNode getDateRangeQuery(int from, int to) {
         return mapper.createObjectNode().set("range",
           mapper.createObjectNode().set("e.ts",
             mapper.createObjectNode().put(GTE,from).put(LTE,to)));
-    }
-
-    private ObjectNode getTermsQuery(ObjectNode query) {
-        if(isDataTypeInteger(query)) {
-            List<Integer> ints = Arrays.stream(query.get(VALUE).textValue().split(","))
-              .map(Integer::parseInt)
-              .collect(Collectors.toList());
-            ArrayNode value = mapper.convertValue(ints,ArrayNode.class);
-            return mapper.createObjectNode().set("terms",
-              mapper.createObjectNode().set("e.".concat(query.get("f").textValue()), value));
-        } else {
-            ArrayNode value = mapper.convertValue(query.get(VALUE).textValue().split(","),ArrayNode.class);
-            return mapper.createObjectNode().set("terms",
-              mapper.createObjectNode().set("e.".concat(query.get("f").textValue()).concat(".keyword"), value));
-        }
-    }
-
-    private ObjectNode getTermQuery(ObjectNode query) {
-        if(isDataTypeInteger(query)) {
-            return mapper.createObjectNode().set("term" ,
-              mapper.createObjectNode().set("e.".concat(query.get("f").textValue()),
-                mapper.createObjectNode().put("value",query.get(VALUE).asInt())));
-        } else {
-            return mapper.createObjectNode().set("term" ,
-              mapper.createObjectNode().set("e.".concat(query.get("f").textValue()).concat(".keyword"),
-                mapper.createObjectNode().put("value",query.get(VALUE).asText())));
-        }
     }
 
     private boolean isDataTypeInteger(JsonNode query) {
@@ -108,16 +89,125 @@ public class ElasticQueryGenerator {
         return "NE".equals(operator);
     }
 
-    private ObjectNode getQueryForNotOperator(String queryType,ObjectNode query) {
+    private ObjectNode getQueryForNotOperator(String queryType,ObjectNode query, String queryBasedOn) {
         if("range".equals(queryType)) {
-            return getRangeQuery(query);
+            return getRangeQuery(query,queryBasedOn);
         } else if ("terms".equals(queryType)) {
-            return getTermsQuery(query);
+            return getTermsQuery(query,queryBasedOn);
         } else if ("term".equals(queryType)) {
-            return getTermQuery(query);
+            return getTermQuery(query,queryBasedOn);
         } else {
-            throw new RuntimeException("No elastic found");
+            throw new RuntimeException("No elastic operation found");
         }
+    }
+
+    private ObjectNode getRangeQuery(ObjectNode query,String queryBasedOn) {
+        final String FILTER_KEY = queryBasedOn.equals(COMMON_PROPS) ? PROP_BASED_FILTER : EVENT_BASED_FILTER;
+        return mapper.createObjectNode().set("range",
+          mapper.createObjectNode().set(FILTER_KEY.concat(query.get("f").textValue()),
+            mapper.createObjectNode().put(query.get("o").textValue().toLowerCase(),query.get(VALUE).asInt())));
+    }
+
+    private ObjectNode getTermsQuery(ObjectNode query,String queryBasedOn) {
+        final String FILTER_KEY = queryBasedOn.equals(COMMON_PROPS) ? PROP_BASED_FILTER : EVENT_BASED_FILTER;
+        ArrayNode value = mapper.convertValue(query.get(VALUE),ArrayNode.class);
+        if(isDataTypeInteger(query)) {
+            return mapper.createObjectNode().set("terms",
+              mapper.createObjectNode().set(FILTER_KEY.concat(query.get("f").textValue()), value));
+        } else {
+            return mapper.createObjectNode().set("terms",
+              mapper.createObjectNode().set(FILTER_KEY.concat(query.get("f").textValue()).concat(".keyword"), value));
+        }
+    }
+
+    private ObjectNode getTermQuery(ObjectNode query,String queryBasedOn) {
+        final String FILTER_KEY = queryBasedOn.equals(COMMON_PROPS) ? PROP_BASED_FILTER : EVENT_BASED_FILTER;
+        if(isDataTypeInteger(query)) {
+            return mapper.createObjectNode().set("term" ,
+              mapper.createObjectNode().set(FILTER_KEY.concat(query.get("f").textValue()),
+                mapper.createObjectNode().put("value",query.get(VALUE).asInt())));
+        } else {
+            return mapper.createObjectNode().set("term" ,
+              mapper.createObjectNode().set(FILTER_KEY.concat(query.get("f").textValue()).concat(".keyword"),
+                mapper.createObjectNode().put("value",query.get(VALUE).asText())));
+        }
+    }
+
+    private ObjectNode getBoolFilteredQuery(ObjectNode jsonRequest) {
+        ArrayNode mustQuery = mapper.createArrayNode();
+        ArrayNode mustNotQuery = mapper.createArrayNode();
+        this.from = Integer.MAX_VALUE;
+        this.to = Integer.MIN_VALUE;
+        Iterator<Map.Entry<String, JsonNode>> it = jsonRequest.fields();
+        while (it.hasNext()) {
+            Map.Entry<String, JsonNode> entry = it.next();
+            ArrayNode queries = (ArrayNode) entry.getValue();
+            int f = Integer.MAX_VALUE;
+            int t = Integer.MIN_VALUE;
+            for(int i = 0; i < queries.size(); i++) {
+                ArrayNode should = mapper.createArrayNode();
+                JsonNode query = queries.get(i);
+                final String QUERY_KEY  = getQueryKey(entry.getKey());
+                if(QUERY_KEY.isEmpty()) {
+                    throw new RuntimeException("Missing query key");
+                }
+                for(int j = 0; j<query.size(); j++) {
+                    JsonNode q = query.get(j);
+                    if(!QUERY_KEY.equals(COMMON_PROPS)) { // COMMON_PROPS don't have keys sT & eT
+                        // Calculating boundaries for buckets
+                        int newF = Math.min(from, q.get("sT").intValue());
+                        int newT = Math.max(to, q.get("eT").intValue());
+                        f = Math.min(newF, f);
+                        t = Math.max(newT, t);
+                    } else {
+                        f = this.from;
+                        t = this.to;
+                    }
+                    ObjectNode newQuery = mapper.createObjectNode();
+                    if (q.size() > 3) {
+                        ArrayNode eachQuery = mapper.createArrayNode();
+                        String qType = "";
+                        if(q.has("o") && !"EQ".equals(q.get("o").textValue()) && !"NE".equals(q.get("o").textValue())) {
+                            qType = "range";
+                            if(!q.get("o").textValue().isEmpty() && !isNotOperator(q.get("o").textValue()) ) {
+                                eachQuery.add(getRangeQuery((ObjectNode) q,QUERY_KEY));
+                            }
+                        } else if (q.has(VALUE) && q.get(VALUE).isArray()) {
+                            qType = "terms";
+                            if (q.has("o") && !isNotOperator(q.get("o").textValue())) {
+                                eachQuery.add(getTermsQuery((ObjectNode) q,QUERY_KEY));
+                            }
+                        } else {
+                            qType = "term";
+                            if (q.has("o") && !isNotOperator(q.get("o").textValue())) {
+                                eachQuery.add(getTermQuery((ObjectNode) q,QUERY_KEY));
+                            }
+                        }
+                        if(eachQuery.size() > 0) {
+                            if(!QUERY_KEY.equals(COMMON_PROPS)) {
+                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
+                                eachQuery.add(getEventQuery(q.get("e").intValue()));
+                            }
+                        }
+                        if (q.has("o") && isNotOperator(q.get("o").textValue())) {
+                            newQuery.set("must",eachQuery);
+                            newQuery.set("must_not",mapper.createArrayNode().add(getQueryForNotOperator(qType, (ObjectNode) q,QUERY_KEY)));
+                        } else {
+                            newQuery.set("bool", mapper.createObjectNode().set("must",eachQuery));
+                        }
+                        should.add(newQuery);
+                    }
+                }
+                if(WHO_DID.equals(QUERY_KEY) || COMMON_PROPS.equals(QUERY_KEY)) {
+                    this.from = Math.min(this.from,f);
+                    this.to = Math.max(this.to,t);
+                    mustQuery.add(mapper.createObjectNode().set("bool",mapper.createObjectNode().set("should",should)));
+                } else if (DID_NOT.equals(QUERY_KEY)) {
+                    mustNotQuery.add(mapper.createObjectNode().set("bool",mapper.createObjectNode().set("should",should)));
+                }
+            }
+        }
+        return createBoolQuery(mustQuery,mustNotQuery);
     }
 
     private ObjectNode createBoolQuery(ArrayNode mustQuery, ArrayNode mustNotQuery) {
@@ -138,88 +228,6 @@ public class ElasticQueryGenerator {
             }
             throw new RuntimeException("Query is empty");
         }
-    }
-
-
-    private ObjectNode getBoolFilteredQuery(ObjectNode jsonRequest) {
-        ArrayNode mustQuery = mapper.createArrayNode();
-        ArrayNode mustNotQuery = mapper.createArrayNode();
-        this.from = Integer.MAX_VALUE;
-        this.to = Integer.MIN_VALUE;
-        Iterator<Map.Entry<String, JsonNode>> it = jsonRequest.fields();
-        while (it.hasNext()) {
-            Map.Entry<String, JsonNode> entry = it.next();
-            ArrayNode queries = (ArrayNode) entry.getValue();
-            int f = Integer.MAX_VALUE;
-            int t = Integer.MIN_VALUE;
-            for(int i = 0; i < queries.size(); i++) {
-                ArrayNode should = mapper.createArrayNode();
-                JsonNode query = queries.get(i);
-                for(int j = 0; j<query.size(); j++) {
-                    JsonNode q = query.get(j);
-                    int newF = Math.min(from, q.get("sT").intValue());
-                    int newT = Math.max(to, q.get("eT").intValue());
-                    f = Math.min(newF, f);
-                    t = Math.max(newT, t);
-                    ObjectNode newQuery = mapper.createObjectNode();
-                    if (q.size() > 3) {
-                        ArrayNode eachQuery = mapper.createArrayNode();
-                        String qType = "";
-                        if(q.has("o") && !"EQ".equals(q.get("o").textValue()) && !"NE".equals(q.get("o").textValue())) {
-                            qType = "range";
-                            if(!q.get("o").textValue().isEmpty()) {
-                                if (isNotOperator(q.get("o").textValue())) {
-                                    eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                    eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                                } else {
-                                    eachQuery.add(getRangeQuery((ObjectNode) q));
-                                    eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                    eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                                }
-                            } else {
-                                eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                            }
-                        } else if (q.has(VALUE) && q.get(VALUE).textValue().contains(",")) {
-                            qType = "terms";
-                            if (q.has("o") && isNotOperator(q.get("o").textValue())) {
-                                eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                            } else {
-                                eachQuery.add(getTermsQuery((ObjectNode) q));
-                                eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                            }
-                        } else {
-                            qType = "term";
-                            if (q.has("o") && isNotOperator(q.get("o").textValue())) {
-                                eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                            } else {
-                                eachQuery.add(getTermQuery((ObjectNode) q));
-                                eachQuery.add(getEventQuery(q.get("e").intValue()));
-                                eachQuery.add(getDateRangeQuery(q.get("sT").intValue(),q.get("eT").intValue()));
-                            }
-                        }
-                        if (q.has("o") && isNotOperator(q.get("o").textValue())) {
-                            newQuery.set("must",eachQuery);
-                            newQuery.set("must_not",mapper.createArrayNode().add(getQueryForNotOperator(qType, (ObjectNode) q)));
-                        } else {
-                            newQuery.set("bool", mapper.createObjectNode().set("must",eachQuery));
-                        }
-                        should.add(newQuery);
-                    }
-                }
-                if("whoDid".equals(entry.getKey()) || "commonProp".equals(entry.getKey())) {
-                    this.from = Math.min(this.from,f);
-                    this.to = Math.max(this.to,t);
-                    mustQuery.add(mapper.createObjectNode().set("bool",mapper.createObjectNode().set("should",should)));
-                } else if ("didNot".equals(entry.getKey())) {
-                    mustNotQuery.add(mapper.createObjectNode().set("bool",mapper.createObjectNode().set("should",should)));
-                }
-            }
-        }
-        return createBoolQuery(mustQuery,mustNotQuery);
     }
 
     private ArrayNode splitDateRangeInNBuckets(int x, int y) {
